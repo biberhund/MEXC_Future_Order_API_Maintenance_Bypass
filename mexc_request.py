f3e90b80d99cdbb0c709ddea7aa55b0bf4b63db1
Ismail Amouma <91847466+ismail-amouma@users.noreply.github.com>
2024-08-27 16:35:26 +0100

    Update mexc_request.py

--- mexc_request.py
+++ mexc_request.py
@@ -1,24 +1,42 @@
-import subprocess
+import hashlib
 import json
+import time
+from curl_cffi import requests
 
-def execute_mexc_request(obj):
+def md5(value):
+    return hashlib.md5(value.encode('utf-8')).hexdigest()
 
-    obj_json = json.dumps(obj)
+def mexc_crypto(key, obj):
+    date_now = str(int(time.time() * 1000))  
+    g = md5(key + date_now)[7:] 
+    s = json.dumps(obj, separators=(',', ':'))  
+    sign = md5(date_now + s + g)  
+    return {'time': date_now, 'sign': sign}
 
-    result = subprocess.run(['node', 'mexc_request.js', obj_json], capture_output=True, text=True)
-
-    return result.stdout
-
-obj = {
-    "symbol": "BTC_USDT",
-    "side": 1,
-    "openType": 1,
-    "type": "1",
-    "vol": 170,
-    "leverage": 20,
-    "price": 2.5,
-    "priceProtect": "0"
+def place_order(key, obj, url):
+    signature = mexc_crypto(key, obj)
+    headers = {
+        'Content-Type': 'application/json',
+        'x-mxc-sign': signature['sign'],
+        'x-mxc-nonce': signature['time'],
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Authorization': key
+    }
+    response = requests.post(url, headers=headers, json=obj)
+    return response.json()
+key = 'WEB...'
+obj = { 
+    "symbol": "BTC_USDT", 
+    "side": 1, 
+    "openType": 1, 
+    "type": "1", 
+    "vol": 170, 
+    "leverage": 20, 
+    "price": 2.5, 
+    "priceProtect": "0" 
 }
 
-response = execute_mexc_request(obj)
+url = 'https://futures.mexc.com/api/v1/private/order/create'  
+
+response = place_order(key, obj, url)
 print(response)
